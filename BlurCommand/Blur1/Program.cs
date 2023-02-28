df0198ae1abbb0dea5311bd47c2202a16795ffd7
Egor <Eg0rMor0zov@yandex.ru>
2023-02-28 22:40:10 +0300

    BlurCommit7

--- BlurCommand/Blur1/Program.cs
+++ BlurCommand/Blur1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.InteropServices;
 
@@ -14,33 +15,40 @@ static void PrintArray(int[] input)
 /*var result11 = TestArray(5);
 PrintArray(result11);*/
 
-var task1Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+/*var task1Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 var result1 = Task1(task1Input);
 Console.Write("TASK 1:\nInput:\n");
 PrintArray(task1Input);
 Console.WriteLine("Result:");
 PrintArray(result1);
 
-var task1ImprovedInput = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+var task1ImprovedInput = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 var result1Improved = Task1Improved(task1ImprovedInput);
 Console.Write("TASK 1 with img size:\nInput:\n");
 PrintArray(task1ImprovedInput);
 Console.WriteLine("Result:");
 PrintArray(result1Improved);
 
-var task2Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-var result2 = Task2(task2Input, 5);
+var task2Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var result2 = Task2(task2Input, 2);
 Console.Write("TASK 2:\nInput:\n");
 PrintArray(task2Input);
 Console.WriteLine("Result:");
 PrintArray(result2);
 
-/*var task3Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+var task2ImprovedInput = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+var result2Improved = Task2Improved(task2ImprovedInput, 2);
+Console.Write("TASK 2 with img size:\nInput:\n");
+PrintArray(task2ImprovedInput);
+Console.WriteLine("Result:");
+PrintArray(result2Improved);*/
+
+var task3Input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 var result3 = Task3(task3Input);
 Console.Write("TASK 3:\nInput:\n");
 PrintArray(task3Input);
 Console.WriteLine("Result:");
-PrintArray(result3);*/
+PrintArray(result3);
 
 /*static int[] TestArray(int size)
 {
@@ -54,7 +62,7 @@ PrintArray(result3);*/
 }*/
 
 //Просто одномерный вариант без блоков
-static int[] Task1(int[] img)
+/*static int[] Task1(int[] img)
 {
     int[] kernel = new int[] { 1, 2, 3, 4, 5, 6, 7 };
     int[] result = new int[img.Length - kernel.Length + 1];
@@ -105,7 +113,7 @@ static int[] Task1Improved(int[] img)
 //Одномерный вариант с блоками
 static int[] Task2(int[] img, int blockSize)
 {
-    int[] kernel = new int[] {1, 2, 3 };
+    int[] kernel = new int[] {1, 2, 3, 4, 5 };
     int[] result = new int[img.Length - kernel.Length * blockSize + blockSize];
     int beginning = kernel.Length / 2;
     int sum = 0;
@@ -129,22 +137,92 @@ static int[] Task2(int[] img, int blockSize)
     return result;
 }
 
-/*static int[] Task3(int[] img)
+//Одномерный вариант c блоками с размером картинки
+static int[] Task2Improved(int[] img, int blockSize)
+{
+    int[] kernel = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+    int[] result = new int[img.Length];
+    int difference = (img.Length - (img.Length - kernel.Length * blockSize + blockSize)) / 2;
+    for (int start = 0; start < difference; start++)
+    {
+        result[start] = img[start];
+    }
+    for (int end = img.Length - difference; end < result.Length; end++)
+    {
+        result[end] = img[end];
+    }
+    int beginning = kernel.Length / 2;
+    int sum = 0;
+    int blockCount = img.Length / blockSize;
+    int numOfBlock = 0;
+    for (int index = beginning; index < (img.Length - kernel.Length * blockSize + blockSize) + beginning; index++)
+    {
+        for (int i = -beginning; i < beginning + 1; i++)
+        {
+            sum += img[index - beginning + numOfBlock] * kernel[beginning + i];
+            if (numOfBlock <= blockCount + 1)
+            {
+                numOfBlock += blockSize;
+            }
+        }
+        sum /= kernel.Length;
+        result[index - beginning + difference] = sum;
+        sum = 0;
+        numOfBlock = 0;
+    }
+    return result;
+}*/
+
+
+//Двумерный вариант без блоков
+static int[] Task3(int[] img)
 {
     int[] kernel = new int[] { 1, 2, 3, 4 };
-    int imgwidth = (int) Math.Sqrt(img.Length);
-    int kernelwidth = (int) Math.Sqrt(kernel.Length);
+    int imgwidth = (int)Math.Sqrt(img.Length);
+    int kernelwidth = (int)Math.Sqrt(kernel.Length);
     var result = new int[(img.Length / imgwidth - (kernel.Length / kernelwidth - 1)) * (img.Length / imgwidth - (kernel.Length / kernelwidth - 1))];
-    for (int index = imgwidth; index < img.Length - imgwidth; index++)
+    int difference = imgwidth - kernelwidth;
+    int sum = 0;
+    int imgIndex = 0;
+    int kernelIndex = 0;
+    int beginningIndex = 0;
+    for (int resultIndex = 0; resultIndex < result.Length; resultIndex++)
     {
-        for (int x = kernelwidth; x < kernel.Length - 1; x++)
+        for (int count = 0; count < kernelwidth; count++)
         {
-            result[index - (img.Length - result.Length - kernelwidth)] =
-            (img[index - imgwidth] * kernel[x - kernelwidth] + img[index - 1 + kernelwidth] * kernel[x] + img[index + 1 + kernelwidth] * kernel[x + 1]) / kernel.Length;
+            for (int i = 0; i < kernelwidth - 1; i++)
+            {
+                sum += img[imgIndex] * kernel[kernelIndex];
+                imgIndex++;
+                kernelIndex++;
+            }
+            if (count != kernelwidth - 1)
+            {
+                sum += img[imgIndex] * kernel[kernelIndex];
+                imgIndex += difference + 1;
+                kernelIndex++;
+            }
+        }
+        sum += img[imgIndex] * kernel[kernelIndex];
+        sum /= kernel.Length;
+        result[resultIndex] = sum;
+        sum = 0;
+        kernelIndex = 0;
+        if ((beginningIndex + difference + 1) % imgwidth == 0)
+        {
+            imgIndex = 0;
+            imgIndex += beginningIndex + kernelwidth;
+            beginningIndex++;
+        }
+        else
+        {
+            imgIndex = 0;
+            beginningIndex++;
+            imgIndex += beginningIndex;
         }
     }
     return result;
-}*///Не доделал
+}
 
 
 
